0e6fc5da1e05e125b16fe9ff5915a4641d0903b0
mohamed <marafath07@gmail.com>
2020-11-22 23:55:43 +0530

    altered the path and rename the dbname

--- src/main/java/com/ipc/userservice/user/controller/AdminController.java
+++ src/main/java/com/ipc/userservice/user/controller/AdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping("AdminController")
+@RequestMapping("/AdminController")
 public class AdminController {
 
     /**
